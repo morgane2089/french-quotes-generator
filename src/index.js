4d48ed15948833adc63a1d2e1a1536f6ede006b9
morgane2089 <93168158+morgane2089@users.noreply.github.com>
2024-04-25 13:26:55 +1000

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function generateQuote (event) {
     event.preventDefault();
     let InstructionInput = document.querySelector("#user-instruction");
     let apiKey ="c91ee5a03a4otdbb84f98dbdea4d441a";
-    let context="You are an AI assistant expert on french quote. Your mission is to generate a 4 line quote in basic HTLM and seperate each line with <br/>. add the quote in coma , Make sure to follow the user instruction, Signe the quote with `SheCodes AI`at the end of the quote inside a <strong> element ";
+    let context="You are an AI assistant expert on french quote. Your mission is to generate a 4 line quote in basic HTLM and seperate each line with <br/>. add the quote in coma , Make sure to follow the user instruction, Signe the quote with `SheCodes AI`at the end of the quote inside a <strong> element in a new ligne ";
     let prompt =`User instruction are : Generate a french quote about ${InstructionInput.value}`;
     
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let quoteElement=document.querySelector("#quote");
+    quoteElement.classList.remove("hidden");
+    quoteElement.innerHTML = `<div class="generating">⏳ Generating a French quote about ${InstructionInput.value}</div>`;
 
     axios.get(apiUrl).then(displayQuote);
 
