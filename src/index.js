29ba999d88528342c39ddf6cc1555605ca0f136b
morgane2089 <93168158+morgane2089@users.noreply.github.com>
2024-04-25 12:55:03 +1000

    Added Axios, API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+function displayQuote(response){
+    new Typewriter('#quote', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor:"",
+      });
+      } 
+
 
 function generateQuote (event) {
     event.preventDefault();
+    let InstructionInput = document.querySelector("#user-instruction");
+    let apiKey ="c91ee5a03a4otdbb84f98dbdea4d441a";
+    let context="You are an AI assistant expert on french quote. Your mission is to generate a 4 line quote in basic HTLM and seperate each line with <br/>. add the quote in coma , Make sure to follow the user instruction, Signe the quote with `SheCodes AI`at the end of the quote inside a <strong> element ";
+    let prompt =`User instruction are : Generate a french quote about ${InstructionInput.value}`;
+    
+    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter('#quote', {
-  strings: "Quote quote quote",
-  autoStart: true,
-  cursor:"",
-});
+    axios.get(apiUrl).then(displayQuote);
+
 }
 
 let quoteFormElement = document.querySelector("#quote-generator");
